d267a7dce1bf5938ca507d8aa39516ae59923e51
YQtheAnalyst <yiqingli07@gmail.com>
2024-06-27 19:10:10 +0100

    add "update" function to UI

--- GLJobTracker/dashboard/urls.py
+++ GLJobTracker/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainList, name="maindashboard")
+    path('', views.mainList, name="maindashboard"),
+    path('update_list/<int:pk>', views.update_list, name="update_list"),
 ]
--- GLJobTracker/dashboard/views.py
+++ GLJobTracker/dashboard/views.py
@@ -21,3 +21,20 @@ def mainList(request):
 
 
     return render(request, template, context)
+
+
+def update_list(request, pk):
+    template = 'dashboard/update_list.html'
+
+    data = models.mainJobList.objects.get(id = pk)
+    form = models.mainJobListForm(instance = data)
+
+    if request.method == "POST":
+        form = models.mainJobListForm(request.POST, instance = data)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, template, context)
